1df24bb9d611914bb72a61f6bb89ddd5a93b4247
Joel Kåberg <joel@kaberg.me>
2019-08-27 10:16:04 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # tExtractor
-Extract torrents upon completion
+Extract torrents upon completion.
+
+The script will recursively scan the `<download_location>/<torrent_name>` folder for compressed archives and extract them to `<download_location>/<torrent_name>/__extracted` (it will also scan the `__extracted` directory for any additional compressed archives).
+
+## Help
+
+The extract.py script has 4 parameters:
+
+Positional parameters:
+Postion 1: torrent hash/id - currently not used
+Postion 2: the torrent name
+Postion 3: the download location
+
+Optional parameters:
+-v / --verbose: debug output
+
+
+## Usage
+While this setup works with most torrent clients, I'll demonstrate the setup with Deluge:
+
+- Fist make sure you have the `Execute` plugin installed
+- Then add a new event on `Torrent Completed` and then enter the location of extract.py in the command box
+- Restart Deluge for settings to take effect
+
+The execute plugin will run any script with the paramters `<script> torrent_id torrent_name download_location`, but you don't need to enter the paramters here
+
+## Extras
+In the folder `extras` within this repository you'll find `sonarr_cleanup` and `radarr_cleanup`. 
+
+To use these scripts setup Sonarr/Radarr via Settings->Connect->Add Connection:
+```
+Name: Cleanup after processing
+On Grab: No
+On Download: Yes
+On Upgrade: Yes
+On Rename: No
+Filter Movie Tags: N/A
+Path: /path/to/script/location`
+```
+This will check and delete the `__extracted` folder if Sonarr/Radarr grabbed the media item from there.
---
+++ extract.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+import os
+import logging
+import argparse
+import subprocess
+
+
+EXTRACT_DIR = '__extracted'
+EXTRACTED_FILES = []
+COMMANDS = {'.rar': 'unrar -r -o- e',
+            '.001': 'unrar -r -o- e',
+            '.zip': 'unzip -u -d'
+           }
+
+
+def find_files(path):
+    found_files = []
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            filename, ext = os.path.splitext(file_path)
+
+            if ext in COMMANDS.keys():
+                if file_path not in EXTRACTED_FILES:
+                    logger.debug('File {0} accepted'.format(filename))
+                    EXTRACTED_FILES.append(file_path)
+                    found_files.append(file_path)
+
+    return found_files
+
+
+def extract(paths, out_dir):
+    for p in paths:
+        logger.info('Extracting: {0}'.format(p))
+
+        filename, ext = os.path.splitext(p)
+        cmd = COMMANDS.get(ext)
+        command = '{0} {1} {2}'.format(cmd, p, out_dir)
+
+        logger.debug('Recived command: {0}'.format(command))
+
+        subprocess.run(command.split())
+
+
+def find_path(rootdir, name):
+    r = os.path.join(rootdir, name)
+
+    if not os.path.isdir(r):
+        r = rootdir
+    extract_dir = os.path.join(r, EXTRACT_DIR)
+
+    logger.info('Output directory is {0}'.format(extract_dir))
+    
+    return extract_dir
+
+
+def main(args):
+    orig_dir = os.path.join(args.path, args.name)
+    out_dir = find_path(args.path, args.name)
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    orig_files = find_files(orig_dir)
+    logger.debug('Files in orginal path {0}'.format(orig_files))
+    extract(orig_files, out_dir)
+
+    found_files = True 
+
+    while found_files:
+        found_files = find_files(out_dir)
+
+        if not found_files:
+            logger.info('Finished processing directory {0}'.format(orig_dir))
+        else:
+            logger.debug('Files in output path {0}'.format(found_files))
+
+            extract(found_files, out_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', type=str, help="The HASH/ID of the torrent")
+    parser.add_argument('name', type=str, help="The name of the torrent")
+    parser.add_argument('path', type=str, help="Absolute path to the root download location")
+    parser.add_argument("-v", "--verbose", help="Set loglevel to debug", action="store_true")
+    args = parser.parse_args()
+
+
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    main(args)
